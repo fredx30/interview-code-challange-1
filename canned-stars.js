0b610d1b9db0fa8615cdc31989a1d212255ddb1a
Fredrik Dyrvold <fredrik@dyrvold.dev>
2021-07-02 14:37:15 +0200

    Add distance functions

--- canned-stars.js
+++ canned-stars.js
@@ -4,7 +4,48 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+function getDistance(point1, point2) {
+    let totalDistance = 0;
+    for (let i = 0; i < 3; i++) {
+        totalDistance += Math.pow(point1[i] - point2[i], 2)
+    }
+    totalDistance = Math.sqrt(totalDistance);
+    return totalDistance;
+}
 
+
+function getLongestDistance(points) {
+    let distanceInfo = [];
+    let longestDistance = 0, x = 0, y = 0;
+    for (let i = 0; i < points.length; i++) {
+        if (!distanceInfo[i]) {
+            distanceInfo[i] = [];
+        }
+        for (let j = 0; j < points[i].length; j++) {
+            if (i === j) continue;
+            distanceInfo[i][j] = getDistance(points[i], points[j]);
+            if (distanceInfo[i][j] > longestDistance) {
+                longestDistance = distanceInfo[i][j];
+                y = i;
+                x = j;
+            }
+        }
+    }
+    return {
+        distance: longestDistance,
+        x: x,
+        y: y,
+    }
+}
+
+const starSet = [
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+    [0, 0, 1],
+];
+const meta = getLongestDistance(starSet);
+console.log(meta);
 
 rl.on('line', (line) => {
     if (line === "exit") {
